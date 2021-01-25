1e3dea05b15a423ed890fc6736061242de40f170
KanuKim97 <74421057+KanuKim97@users.noreply.github.com>
2021-01-25 23:33:22 +0900

    Create_Express_login3.html
    Create_Express_login3.html

--- ExpressExample/ExpressExample7-2.js
+++ ExpressExample/ExpressExample7-2.js
@@ -7,6 +7,8 @@ var bodyParser = require('body-parser'),
 
 var app = express();
 
+var router = express.Router();
+
 app.set('port', process.env.PORT || 3000);
 
 http.createServer(app).listen(app.get('port'), function () {
@@ -21,21 +23,22 @@ app.use(bodyParser.json());
 
 app.use('/public', static(path.join(__dirname, 'public')));
 
-var router = express.Router();
+router.route('/process/login/:name').post(function (req, res) {
+    console.log('/process/login/:name 처리함');
 
-router.route('/process/login').post(function(req, res){
-    console.log('/process/login 처리함');
-    
+    var paramName = req.params.name;
+
     var paramId = req.body.id || req.query.id;
     var paramPassword = req.body.password;
-    
+
     res.writeHead('200', {
         'Content-Type': 'text/html;charset=utf8'
     });
     res.write('<h1>Express서버에서 응답한 결과입니다.</h1>');
+    res.write('<div><p>Param-Name:' + paramName + '</p></div>');
     res.write('<div><p>Param-Id:' + paramId + '</p></div>');
     res.write('<div><p>Param-Password:' + paramPassword + '</p></div>');
-    res.write("<br><br><a href='/public/login2.html'>로그인 페이지로 돌아가기</a>");
+    res.write("<br><br><a href='/public/login3.html'>로그인 페이지로 돌아가기</a>");
     res.end();
 });
 
